09367d4b845862481d39734d5303ae54309b5bd7
Micah Martin <micahlmartin@gmail.com>
2021-06-21 14:59:21 -0400

    Updated

--- utils/build.sh
+++ utils/build.sh
@@ -2,13 +2,18 @@
 
 function preprelease() {
   version=${VERSION:?"VERSION must be set"}
+  release_branch="release/$version"
 
-  git checkout -b release/$version
+  git checkout -b $release_branch
   
   sed -i.bak "s/SDKVersion = .*/SDKVersion = \"$version\"/"  harness/version.go && rm harness/version.go.bak
 
   git branch
   git status
+
+  git add -A
+  git commit -am"Release v${version} $(date +%m-%d-%Y)"
+  git push origin $release_branch
 }
 
 $1
