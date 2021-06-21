94c6db8fcb4e558fbd091cd5e5009dd1b45d29c4
Micah Martin <micahlmartin@gmail.com>
2021-06-21 14:50:27 -0400

    testing

--- utils/build.sh
+++ utils/build.sh
@@ -3,9 +3,12 @@
 function preprelease() {
   version=${VERSION:?"VERSION must be set"}
 
-  git co -b release/$version
+  git checkout -b release/$version
   
   sed -i.bak "s/SDKVersion = .*/SDKVersion = \"$version\"/"  harness/version.go && rm harness/version.go.bak
+
+  git branch
+  git status
 }
 
 $1
