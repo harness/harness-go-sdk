46850d27b360efe3e75af7cb1551650d75bd6563
Micah Martin <micahlmartin@gmail.com>
2021-06-21 15:16:55 -0400

    testing

--- utils/build.sh
+++ utils/build.sh
@@ -2,20 +2,20 @@
 
 function preprelease() {
   version=${VERSION:?"VERSION must be set"}
-  release_branch="release/$version"
+  # release_branch="release/$version"
 
-  git checkout -b $release_branch
-  git config user.email "harness-ci-automation@harness.io"
-  git config user.name "Harness CI Automation"
+  # git checkout -b $release_branch
+  # git config user.email "harness-ci-automation@harness.io"
+  # git config user.name "Harness CI Automation"
   sed -i.bak "s/SDKVersion = .*/SDKVersion = \"$version\"/"  harness/version.go && rm harness/version.go.bak
 
-  git branch
-  git status
-  cat .git/config
-  
-  git add -A
-  git commit -am"Release v${version} $(date +%m-%d-%Y)"
-  git push origin $release_branch
+  # git branch
+  # git status
+  # cat .git/config
+
+  # git add -A
+  # git commit -am"Release v${version} $(date +%m-%d-%Y)"
+  # git push origin $release_branch
 }
 
 $1
