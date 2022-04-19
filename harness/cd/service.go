863b191e71b03e8788a79ee7ef8125c32e8a4506
Micah Martin <micahlmartin@gmail.com>
2022-04-19 15:20:56 -0500

    Reworked service methods

--- harness/cd/service.go
+++ harness/cd/service.go
@@ -2,6 +2,7 @@ package cd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/harness/harness-go-sdk/harness/cd/graphql"
 )
@@ -10,21 +11,20 @@ type ServiceClient struct {
 	ApiClient *ApiClient
 }
 
-func (ac *ServiceClient) ListServices(limit int, offset int) ([]*graphql.Service, *graphql.PageInfo, error) {
-	return ac.listServicesInternal(limit, offset, "[]")
-}
-
-func (ac *ServiceClient) listServicesInternal(limit int, offset int, filters string) ([]*graphql.Service, *graphql.PageInfo, error) {
+func (ac *ServiceClient) ListServices(limit int, offset int, filters []*graphql.ServiceFilter) ([]*graphql.Service, *graphql.PageInfo, error) {
 
 	query := &GraphQLQuery{
-		Query: fmt.Sprintf(`query {
-			services(limit: %[3]d, offset: %[4]d, filters: %[5]s) {
+		Query: fmt.Sprintf(`query($filters: [ServiceFilter]) {
+			services(limit: %[3]d, offset: %[4]d. filters: $filters) {
 				nodes {
 					%[1]s
 				}
 				%[2]s
 			}
-		}`, standardServiceFields, paginationFields, limit, offset, filters),
+		}`, standardServiceFields, paginationFields, limit, offset),
+		Variables: map[string]interface{}{
+			"filters": filters,
+		},
 	}
 
 	res := struct {
@@ -44,15 +44,63 @@ func (ac *ServiceClient) listServicesInternal(limit int, offset int, filters str
 }
 
 func (ac *ServiceClient) ListServicesByApplicationId(appId string, limit int, offset int) ([]*graphql.Service, *graphql.PageInfo, error) {
+	filter := []*graphql.ServiceFilter{
+		{
+			Application: &graphql.IdFilter{
+				Operator: graphql.IdOperatorTypes.Equals,
+				Values:   []string{appId},
+			},
+		},
+	}
+	return ac.ListServices(limit, offset, filter)
+}
 
-	filterString := fmt.Sprintf(`[{
-		application: {
-			operator: %[1]s,
-			values: ["%[2]s"]
+func (ac *ServiceClient) GetServiceById(id string) (*graphql.Service, error) {
+	query := &GraphQLQuery{
+		Query: fmt.Sprintf(`query($id: String!) {
+			service(serviceId: $id) {
+				%s
+			}
+		}`, standardServiceFields),
+		Variables: map[string]interface{}{
+			"id": id,
+		},
+	}
+
+	res := struct {
+		Service graphql.Service
+	}{}
+	err := ac.ApiClient.ExecuteGraphQLQuery(query, &res)
+
+	if err != nil {
+		return nil, nil
+	}
+
+	return &res.Service, nil
+}
+
+func (ac *ServiceClient) GetServiceByName(appId string, name string) (*graphql.Service, error) {
+	limit := 25
+	offset := 0
+	hasMore := true
+
+	for hasMore {
+		services, pagination, err := ac.ListServicesByApplicationId(appId, limit, offset)
+		if err != nil {
+			return nil, err
 		}
-	}]`, graphql.IdOperatorTypes.Equals, appId)
 
-	return ac.listServicesInternal(limit, offset, filterString)
+		for _, service := range services {
+			if strings.EqualFold(service.Name, name) {
+				return service, nil
+			}
+		}
+
+		hasMore = pagination.HasMore
+		offset += limit
+	}
+
+	return nil, nil
 }
 
 const (
--- harness/cd/service_test.go
+++ harness/cd/service_test.go
@@ -13,7 +13,7 @@ func TestListServices(t *testing.T) {
 	hasMore := true
 
 	for hasMore {
-		services, pagination, err := c.ServiceClient.ListServices(limit, offset)
+		services, pagination, err := c.ServiceClient.ListServices(limit, offset, nil)
 		require.NoError(t, err, "Failed to list services: %s", err)
 		require.NotEmpty(t, services, "No services found")
 		require.NotNil(t, pagination, "Pagination should not be nil")
@@ -26,7 +26,7 @@ func TestListServices(t *testing.T) {
 func TestListServicesByApplicationId(t *testing.T) {
 	c := getClient()
 
-	app, err := c.ApplicationClient.GetApplicationByName("Harness Demo Application")
+	app, err := c.ApplicationClient.GetApplicationByName("Harness Automation Example")
 	require.NoError(t, err, "Failed to get application: %s", err)
 
 	limit := 100
@@ -42,4 +42,37 @@ func TestListServicesByApplicationId(t *testing.T) {
 		hasMore = len(services) == limit
 		offset += limit
 	}
+}
+
+func TestGraphqlGetServiceById(t *testing.T) {
+	c := getClient()
+
+	app, err := c.ApplicationClient.GetApplicationByName("Harness Automation Example")
+	require.NoError(t, err, "Failed to get application: %s", err)
+
+	services, pagination, err := c.ServiceClient.ListServicesByApplicationId(app.Id, 1, 0)
+	require.NoError(t, err, "Failed to list services: %s", err)
+	require.NotEmpty(t, services, "No services found")
+	require.NotNil(t, pagination, "Pagination should not be nil")
+
+	svc := services[0]
+
+	foundSvc, err := c.ServiceClient.GetServiceById(svc.Id)
+	require.NoError(t, err, "Failed to get service: %s", err)
+	require.NotNil(t, foundSvc, "Service should not be nil")
+	require.Equal(t, svc.Id, foundSvc.Id)
+}
+
+func TestGraphqlGetServiceByName(t *testing.T) {
+	c := getClient()
+
+	svcName := "nginx"
+
+	app, err := c.ApplicationClient.GetApplicationByName("Harness Automation Example")
+	require.NoError(t, err, "Failed to get application: %s", err)
+
+	foundSvc, err := c.ServiceClient.GetServiceByName(app.Id, svcName)
+	require.NoError(t, err, "Failed to get service: %s", err)
+	require.NotNil(t, foundSvc, "Service should not be nil")
+	require.Equal(t, svcName, foundSvc.Name)
 }
