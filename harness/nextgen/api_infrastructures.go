5a153c0abb9517e3c2d951e159daa7a94c1fecc6
Vivek Dixit <vivek.dixit@harness.io>
2024-03-07 13:08:37 +0530

    Added API key

--- harness/nextgen/api_infrastructures.go
+++ harness/nextgen/api_infrastructures.go
@@ -776,6 +776,20 @@ func (a *InfrastructuresApiService) ImportInfrastructure(ctx context.Context, ac
 	if localVarHttpHeaderAccept != "" {
 		localVarHeaderParams["Accept"] = localVarHttpHeaderAccept
 	}
+	if ctx != nil {
+		// API Key Authentication
+		if auth, ok := ctx.Value(ContextAPIKey).(APIKey); ok {
+			var key string
+			if auth.Prefix != "" {
+				key = auth.Prefix + " " + auth.Key
+			} else {
+				key = auth.Key
+			}
+			localVarHeaderParams["x-api-key"] = key
+
+		}
+	}
+
 	r, err := a.client.prepareRequest(ctx, localVarPath, localVarHttpMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
 	if err != nil {
 		return localVarReturnValue, nil, err
