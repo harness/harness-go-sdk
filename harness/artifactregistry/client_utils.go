f67305cd074fa4e20976464998c7b8f61818f6d1
Ritek Rounak <ritek.rounak@harness.io>
2024-12-07 13:58:54 +0530

    feat: [AH-758]: Onboard Harness Artifact Registry

--- harness/artifactregistry/client_utils.go
+++
@@ -1,8 +0,0 @@
-package artifactregistry
-
-import "context"
-
-func (c *APIClient) WithAuthContext(ctx context.Context) (*APIClient, context.Context) {
-	authCtx := context.WithValue(ctx, ContextAPIKey, APIKey{Key: c.ApiKey})
-	return c, authCtx
-}
