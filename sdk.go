247da8087bcca6f8f7c463ff06dd851164974055
Arvind Choudhary <arvind.choudhary@harness.io>
2025-07-22 19:08:35 -0700

    [AH-1611]: SDK update

--- sdk.go
+++ sdk.go
@@ -3,7 +3,6 @@ package sdk
 import (
 	_ "github.com/harness/harness-go-sdk/harness/cd"
 	_ "github.com/harness/harness-go-sdk/harness/delegate"
-	_ "github.com/harness/harness-go-sdk/harness/har"
 	_ "github.com/harness/harness-go-sdk/harness/helpers"
 	_ "github.com/harness/harness-go-sdk/harness/nextgen"
 	_ "github.com/harness/harness-go-sdk/harness/time"
