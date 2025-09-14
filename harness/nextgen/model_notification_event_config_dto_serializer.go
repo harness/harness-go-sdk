87c10bddee77a851a4b40329d1b789b96107987e
akshatgoyal <akshat.goyal@harness.io>
2025-09-14 19:19:11 +0530

    #bugFix

--- harness/nextgen/model_notification_event_config_dto_serializer.go
+++ harness/nextgen/model_notification_event_config_dto_serializer.go
@@ -44,15 +44,20 @@ func (a *NotificationEventConfigDto) UnmarshalJSON(data []byte) error {
 
 	switch *probe.Type_ {
 	case DELEGATE_ResourceTypeEnum:
-		err = json.Unmarshal(aux.NotificationEventData, &a.DelegateEventNotificationParamsDto)
+		a.DelegateEventNotificationParamsDto = &DelegateEventNotificationParamsDto{}
+		err = json.Unmarshal(aux.NotificationEventData, a.DelegateEventNotificationParamsDto)
 	case PIPELINE_ResourceTypeEnum:
-		err = json.Unmarshal(aux.NotificationEventData, &a.PipelineEventNotificationParamsDto)
+		a.PipelineEventNotificationParamsDto = &PipelineEventNotificationParamsDto{}
+		err = json.Unmarshal(aux.NotificationEventData, a.PipelineEventNotificationParamsDto)
 	case CHAOS_EXPERIMENT_ResourceTypeEnum:
-		err = json.Unmarshal(aux.NotificationEventData, &a.ChaosExperimentEventNotificationParamsDto)
+		a.ChaosExperimentEventNotificationParamsDto = &ChaosExperimentEventNotificationParamsDto{}
+		err = json.Unmarshal(aux.NotificationEventData, a.ChaosExperimentEventNotificationParamsDto)
 	case SERVICE_LEVEL_OBJECTIVE_ResourceTypeEnum:
-		err = json.Unmarshal(aux.NotificationEventData, &a.SloEventNotificationParamsDto)
+		a.SloEventNotificationParamsDto = &SloEventNotificationParamsDto{}
+		err = json.Unmarshal(aux.NotificationEventData, a.SloEventNotificationParamsDto)
 	case STO_EXEMPTION_ResourceTypeEnum:
-		err = json.Unmarshal(aux.NotificationEventData, &a.StoExemptionEventNotificationParamsDto)
+		a.StoExemptionEventNotificationParamsDto = &StoExemptionEventNotificationParamsDto{}
+		err = json.Unmarshal(aux.NotificationEventData, a.StoExemptionEventNotificationParamsDto)
 	default:
 		return fmt.Errorf("unknown resource type %s", *probe.Type_)
 	}
@@ -63,44 +68,38 @@ func (a *NotificationEventConfigDto) UnmarshalJSON(data []byte) error {
 func (a *NotificationEventConfigDto) MarshalJSON() ([]byte, error) {
 	type Alias NotificationEventConfigDto
 
-	// If NotificationEventData is already populated, use it directly
+	// Always use the original NotificationEventData if it's available
 	if len(a.NotificationEventData) > 0 {
-		// Check if it's null
-		if len(a.NotificationEventData) == 4 && string(a.NotificationEventData) == "null" {
-			// Preserve null as-is
-			return json.Marshal((*Alias)(a))
-		}
-
-		// For non-null data, try to marshal the specific DTO if it's populated
-		var notification_event_data []byte
-		var err error
-
-		// Check which specific DTO is populated and marshal that
-		if a.PipelineEventNotificationParamsDto != nil {
-			notification_event_data, err = json.Marshal(a.PipelineEventNotificationParamsDto)
-		} else if a.DelegateEventNotificationParamsDto != nil {
-			notification_event_data, err = json.Marshal(a.DelegateEventNotificationParamsDto)
-		} else if a.ChaosExperimentEventNotificationParamsDto != nil {
-			notification_event_data, err = json.Marshal(a.ChaosExperimentEventNotificationParamsDto)
-		} else if a.SloEventNotificationParamsDto != nil {
-			notification_event_data, err = json.Marshal(a.SloEventNotificationParamsDto)
-		} else if a.StoExemptionEventNotificationParamsDto != nil {
-			notification_event_data, err = json.Marshal(a.StoExemptionEventNotificationParamsDto)
-		} else {
-			// No specific DTO is populated, use the original NotificationEventData
-			notification_event_data = a.NotificationEventData
-		}
-
-		if err != nil {
-			return nil, err
-		}
-
 		// Create a copy to avoid modifying the original
 		result := *a
-		result.NotificationEventData = json.RawMessage(notification_event_data)
 		return json.Marshal((*Alias)(&result))
 	}
 
-	// If NotificationEventData is empty, return error
-	return nil, fmt.Errorf("notification_event_data is empty")
+	// If NotificationEventData is empty, try to reconstruct from specific DTOs
+	var notification_event_data []byte
+	var err error
+
+	// Check which specific DTO is populated and marshal that
+	if a.PipelineEventNotificationParamsDto != nil {
+		notification_event_data, err = json.Marshal(a.PipelineEventNotificationParamsDto)
+	} else if a.DelegateEventNotificationParamsDto != nil {
+		notification_event_data, err = json.Marshal(a.DelegateEventNotificationParamsDto)
+	} else if a.ChaosExperimentEventNotificationParamsDto != nil {
+		notification_event_data, err = json.Marshal(a.ChaosExperimentEventNotificationParamsDto)
+	} else if a.SloEventNotificationParamsDto != nil {
+		notification_event_data, err = json.Marshal(a.SloEventNotificationParamsDto)
+	} else if a.StoExemptionEventNotificationParamsDto != nil {
+		notification_event_data, err = json.Marshal(a.StoExemptionEventNotificationParamsDto)
+	} else {
+		return nil, fmt.Errorf("notification_event_data is empty and no specific DTO is populated")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a copy to avoid modifying the original
+	result := *a
+	result.NotificationEventData = json.RawMessage(notification_event_data)
+	return json.Marshal((*Alias)(&result))
 }
