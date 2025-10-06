aad1e2988ad88dc142ccd96623519bdecc80ef26
Shivam  Thakur <shivam.thakur@harness.io>
2025-10-06 11:11:33 +0000

    feat: [CCM-26310]: SDK Changes for calling ToggleClusterOrchestratorState API (#690)
    * 5f7279 added ToggleClusterOrchestratorState method

--- harness/nextgen/api_cloud_cost_cluster_orchestrator.go
+++ harness/nextgen/api_cloud_cost_cluster_orchestrator.go
@@ -267,3 +267,90 @@ func (a *CloudCostClusterOrchestratorApiService) UpdateClusterOrchestratorConfig
 
 	return localVarReturnValue, localVarHttpResponse, nil
 }
+
+func (a *CloudCostClusterOrchestratorApiService) ToggleClusterOrchestratorState(ctx context.Context, accountIdentifier string, orchID string, disabled bool) (ToggleStateResponse, *http.Response, error) {
+	var (
+		localVarHttpMethod  = strings.ToUpper("Put")
+		localVarPostBody    interface{}
+		localVarFileName    string
+		localVarFileBytes   []byte
+		localVarReturnValue ToggleStateResponse
+	)
+
+	localVarPath := fmt.Sprintf("%s/lw/api/accounts/%s/clusters/orchestrator/%s/toggle_state", a.client.cfg.BasePath, accountIdentifier, orchID)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	localVarQueryParams.Add("accountIdentifier", parameterToString(accountIdentifier, ""))
+	localVarQueryParams.Add("disabled", parameterToString(disabled, ""))
+	// to determine the Content-Type header
+	localVarHttpContentTypes := []string{"application/json"}
+
+	// set Content-Type header
+	localVarHttpContentType := selectHeaderContentType(localVarHttpContentTypes)
+	if localVarHttpContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHttpContentType
+	}
+
+	// to determine the Accept header
+	localVarHttpHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHttpHeaderAccept := selectHeaderAccept(localVarHttpHeaderAccepts)
+	if localVarHttpHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHttpHeaderAccept
+	}
+	// body params
+	localVarPostBody = nil
+	if ctx != nil {
+		// API Key Authentication
+		if auth, ok := ctx.Value(ContextAPIKey).(APIKey); ok {
+			var key string
+			if auth.Prefix != "" {
+				key = auth.Prefix + " " + auth.Key
+			} else {
+				key = auth.Key
+			}
+			localVarHeaderParams["x-api-key"] = key
+
+		}
+	}
+	r, err := a.client.prepareRequest(ctx, localVarPath, localVarHttpMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHttpResponse, err := a.client.callAPI(r)
+	if err != nil || localVarHttpResponse == nil {
+		return localVarReturnValue, localVarHttpResponse, err
+	}
+
+	localVarBody, err := ioutil.ReadAll(localVarHttpResponse.Body)
+	localVarHttpResponse.Body.Close()
+	if err != nil {
+		return localVarReturnValue, localVarHttpResponse, err
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+	if localVarHttpResponse.StatusCode < 300 {
+		// If we succeed, return the data, otherwise pass on to decode error.
+		return localVarReturnValue, localVarHttpResponse, err
+	}
+
+	if localVarHttpResponse.StatusCode >= 300 {
+		newErr := GenericSwaggerError{
+			body:  localVarBody,
+			error: localVarHttpResponse.Status,
+		}
+		if err != nil {
+			newErr.error = err.Error()
+			return localVarReturnValue, localVarHttpResponse, newErr
+		}
+		newErr.error = strings.Join(localVarReturnValue.Errors, ", ")
+		return localVarReturnValue, localVarHttpResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHttpResponse, nil
+}
