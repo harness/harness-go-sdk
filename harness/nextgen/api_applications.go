632f989fe47793eafea68688d5cdd77a8595b0f1
Manish Singh <manish.singh@harness.io>
2026-06-29 12:21:35 +0000

    feat: [CDS-121707]: Add force delete to GitOps app (#744)
    * 8f9795 feat: [CDS-121707]: Add force delete to GitOps app

--- harness/nextgen/api_applications.go
+++ harness/nextgen/api_applications.go
@@ -210,6 +210,7 @@ Delete deletes an application.
      * @param "RequestAppNamespace" (optional.String) -
      * @param "RequestProject" (optional.String) -
      * @param "OptionsRemoveExistingFinalizers" (optional.Bool) -
+     * @param "OptionsForceDelete" (optional.Bool) -
 @return ApplicationsApplicationResponse
 */
 
@@ -222,6 +223,7 @@ type ApplicationsApiAgentApplicationServiceDeleteOpts struct {
 	RequestAppNamespace             optional.String
 	RequestProject                  optional.String
 	OptionsRemoveExistingFinalizers optional.Bool
+	OptionsForceDelete              optional.Bool
 }
 
 func (a *ApplicationsApiService) AgentApplicationServiceDelete(ctx context.Context, agentIdentifier string, requestName string, localVarOptionals *ApplicationsApiAgentApplicationServiceDeleteOpts) (ApplicationsApplicationResponse, *http.Response, error) {
@@ -266,6 +268,9 @@ func (a *ApplicationsApiService) AgentApplicationServiceDelete(ctx context.Conte
 	}
 	if localVarOptionals != nil && localVarOptionals.OptionsRemoveExistingFinalizers.IsSet() {
 		localVarQueryParams.Add("options.removeExistingFinalizers", parameterToString(localVarOptionals.OptionsRemoveExistingFinalizers.Value(), ""))
+	}
+	if localVarOptionals != nil && localVarOptionals.OptionsForceDelete.IsSet() {
+		localVarQueryParams.Add("options.forceDelete", parameterToString(localVarOptionals.OptionsForceDelete.Value(), ""))
 	}
 	// to determine the Content-Type header
 	localVarHttpContentTypes := []string{}
